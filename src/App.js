f96be4c6e52d30a471c84f56592d07bb069bd6db
Elias <eliasker@gmail.com>
2020-03-14 15:46:56 +0200

    testing random background colors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [colors, setColors] = useState([])
+  const possibleColors = ['black', 'silver', 'gray', 'maroon', 'red',
+    'purple', 'fuchsia', 'green', 'lime', 'olive',
+    'yellow', 'navy', 'blue', 'teal', 'aqua']
+
+  const generateStyleObj = index => {
+    const styleObj = {
+      background: colors[index],
+    }
+    return styleObj
+  }
+
+  const handleRandomize = () => {
+    const newColors = [
+      possibleColors[Math.floor(Math.random() * possibleColors.length)],
+      possibleColors[Math.floor(Math.random() * possibleColors.length)],
+      possibleColors[Math.floor(Math.random() * possibleColors.length)]
+    ]
+    setColors(newColors)
+  }
+
+  const Row = () => {
+    return (
+      <div className='color-row'>
+        <span style={generateStyleObj(0)}>foo</span>
+        <span style={generateStyleObj(1)}>bar</span>
+        <span style={generateStyleObj(2)}>baz</span>
+      </div>
+    )
+  }
   return (
     <div className='App'>
-      <p>hello world</p>
+      <Row />
+      <button onClick={handleRandomize}>Randomize</button>
     </div>
   );
 }
